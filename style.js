4b324b8e7c88bc2acab70c0d591958c8a2da81f8
Iori Kojima <kojima.iori@gmail.com>
2023-10-19 16:13:32 +0900

    a

--- style.js
+++ style.js
@@ -1,42 +1,28 @@
-const numSquares = 1000;
-const squares = [];
-//const centerOffset = 150; // 円の中心から表示しない範囲
-const minSpeed = 0.0008; // 最小移動スピード
-const maxSpeed = 0.011; // 最大移動スピード
+const animationContainer = document.getElementById("animation-container");
 
-for (let i = 0; i < numSquares; i++) {
+function createSquare() {
     const square = document.createElement('div');
     square.classList.add('square');
+    animationContainer.appendChild(square);
+    const startY = Math.random() * (animationContainer.offsetHeight - 2);
+    square.style.top = `${startY}px`;
+    square.style.left = '0px';
+    const duration = Math.random() * 5 + 1;
+    square.style.transition = `left ${duration}s linear`;
 
-    //const radius = Math.random() * (window.innerWidth / 2.3 - centerOffset) + centerOffset;
-    const radius = 200 + Math.random() * 150; // 150 200  200 100
-    const angle = Math.random() * Math.PI * 2;
-    const speed = Math.random() * (maxSpeed - minSpeed) + minSpeed;
-    square.style.left = `${window.innerWidth / 2 + radius * Math.cos(angle)}px`;
-    square.style.top = `${window.innerHeight / 2 + radius * Math.sin(angle)}px`;
+    setTimeout(() => {
+        square.style.left = `${animationContainer.offsetWidth}px`;
+    }, 0);
 
-    document.body.appendChild(square);
-    squares.push({ element: square, radius, angle, speed });
+    setTimeout(() => {
+        animationContainer.removeChild(square);
+    }, duration * 5000);
 }
 
-function updateCenter() {
-    squares.forEach(square => {
-        square.element.style.left = `${window.innerWidth / 2 + square.radius * Math.cos(square.angle)}px`;
-        square.element.style.top = `${window.innerHeight / 2 + square.radius * Math.sin(square.angle)}px`;
-    });
+function generateSquares() {
+    createSquare();
+    setTimeout(generateSquares, 0.001); //0001
 }
 
-function animate() {
-    squares.forEach(square => {
-        square.angle += square.speed;
-        square.element.style.left = `${window.innerWidth / 2 + square.radius * Math.cos(square.angle)}px`;
-        square.element.style.top = `${window.innerHeight / 2 + square.radius * Math.sin(square.angle)}px`;
-    });
+generateSquares();
 
-    requestAnimationFrame(animate);
-}
-
-window.addEventListener('resize', updateCenter);
-window.addEventListener('scroll', updateCenter);
-
-animate();
