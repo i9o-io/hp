f8e3edfc69761dc5157548037f1c6556afa83672
Iori Kojima <kojima.iori@gmail.com>
2023-10-19 18:19:29 +0900

    f

--- style.js
+++ style.js
@@ -1,10 +1,13 @@
-// 正方形の数と生成間隔を設定
-const generateInterval = 5;  // 100ミリ秒ごとに新しい正方形を生成
+// 定数と変数の設定
+const generateInterval = 50;
+let isCircularMotion = false;
+let lastSwitchTime = Date.now();
+let generator;
 
 // 新しい正方形を生成
 function generateSquare() {
     createSquare();
-    setTimeout(generateSquare, generateInterval);
+    generator = setTimeout(generateSquare, generateInterval);
 }
 
 // アニメーションを開始
@@ -15,36 +18,56 @@ function createSquare() {
     square.classList.add('square');
     document.body.appendChild(square);
 
-    // ランダムな速度を生成 (例: 0.5 から 2.5 の範囲で)
-    const speed = Math.random() * 10 + 1;
+    const speed = Math.random() * 8 + 2;
+    const centerX = window.innerWidth / 2;
+    const centerY = window.innerHeight / 2;
+    let angle = 0;
 
     resetSquare(square);
 
-    // 正方形を動かす
     function moveSquare() {
-        const rect = square.getBoundingClientRect();
-        if (rect.right < window.innerWidth) {
-            square.style.left = rect.left + speed + 'px';
-            requestAnimationFrame(moveSquare);
-        } else {
-            resetSquare(square);
-            requestAnimationFrame(moveSquare);
+        const elapsedTime = (Date.now() - lastSwitchTime) / 1000;
+        if (elapsedTime >= 8) {
+            isCircularMotion = !isCircularMotion;
+            lastSwitchTime = Date.now();
         }
+
+        if (isCircularMotion) {
+            const radius = Math.sqrt(Math.pow(centerX - square.offsetLeft, 2) + Math.pow(centerY - square.offsetTop, 2));
+            angle += speed / radius;
+            square.style.left = centerX + radius * Math.cos(angle) - 1 + 'px';
+            square.style.top = centerY + radius * Math.sin(angle) - 1 + 'px';
+        } else {
+            square.style.left = square.offsetLeft + speed + 'px';
+        }
+
+        requestAnimationFrame(moveSquare);
     }
 
     moveSquare();
 }
 
-// 正方形の位置と速度をリセット
 function resetSquare(square) {
     const y = Math.floor(Math.random() * (window.innerHeight + window.scrollY));
     square.style.left = -2 + 'px';
     square.style.top = y + 'px';
 }
 
-// 以下の行を削除
-// window.onscroll = function () {
-//     document.querySelectorAll('.square').forEach(square => {
-//         resetSquare(square);
-//     });
-// }
+// アニメーションをリセット
+function resetAnimation() {
+    // 既存の正方形を削除
+    const squares = document.querySelectorAll('.square');
+    squares.forEach(square => {
+        document.body.removeChild(square);
+    });
+    // 正方形の生成を停止
+    clearTimeout(generator);
+    // 変数をリセット
+    isCircularMotion = false;
+    lastSwitchTime = Date.now();
+    // アニメーションを再開
+    generateSquare();
+}
+
+// 16秒ごとにアニメーションをリセット
+setInterval(resetAnimation, 16000);
